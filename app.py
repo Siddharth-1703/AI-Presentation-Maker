301bc8a8710379a94e906f68633aa3028b4a7fc8
Siddharth-1703 <khuranasiddharth35@gmail.com>
2026-08-01 11:08:38 +0530

    Fix indentation for exception handling in app.py

--- app.py
+++ app.py
@@ -154,7 +154,7 @@ if (user_input):
                                 file_name = 'ppt.html',
                                 mime= 'text/html'):
               st.success("PPt Downloaded Successfully!!")
-          except Exception as err:
+        except Exception as err:
             st.error("Error Code: " ,err)
            
     
